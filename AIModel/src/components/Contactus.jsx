3fe30cb1c8eea2837a8be2774a51a42450ab2650
AvaniSaxena-git <avanisaxena2005@gmail.com>
2025-01-31 19:28:12 +0530

    update2

--- AIModel/src/components/Contactus.jsx
+++ AIModel/src/components/Contactus.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 function Contactus(){
 return(
-    <div className=" justify-center flex items-center text-center  h-screen bg-cover bg-center bg-cyan-950 bg-no-repeat ">
+<div className=" justify-center bg-cyan-950 items-center  bg-cover h-screen ">
+<div className="text-gray-50 font-bold from-stone-800 text-7xl mb-[200px]  ml-[600px]  ">
+Contact Us
+</div>
+
+<div className="text-gray-50 mt-[20vh]  mr-[400px] ml-44 font-medium text-4xl">Address
+    <p className="text-gray-50 text-xl mt-4 w-48 font-light">BN-56 <br/> Narela 462041 , Bhopal MP </p>
     
-   <div className="text-gray-50 text-6xl mt-16 font-bold">
-      Contact Us <P className=""> BN-56 Narela , 465667 ,Bhoplal </P>
-   </div>
-   <div className=" text-xl mt-4 font-semibold text-gray-900  ">
-       Address
-   </div>
-   
     </div>
+    
+    <div className="text-gray-50  mt-[0vh] ml-[90vh] font-medium text-4xl">Phone
+    <p className="text-gray-50 text-xl mt-4 w-48 font-light">8435106498,</p>  </div>
+
+ <div className="text-gray-50  ml-[150vh] mb-[400px] font-medium text-4xl">Email
+    <p className="text-gray-50 text-xl mt-4 w-48 font-light">avanisaxena2005@gmail.com  </p>
+</div>
+</div>
 )
-
-
-}export default Contactus
+}
+export default Contactus
