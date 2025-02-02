0ee567f022981c4944dc40ad498ac341b5fc331f
AvaniSaxena-git <avanisaxena2005@gmail.com>
2025-02-02 15:20:05 +0530

    pull2feb

--- AIModel/src/components/Navbar.jsx
+++ AIModel/src/components/Navbar.jsx
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom"; // If you're using react-router-dom for
 const Navbar = () => {
   return (
     <div className="">
-      <nav className="bg-cyan-950 p-9">
-        <div className="max-w-7xl text-2xl mx-auto flex justify-between items-center">
-          <div className="text-white text-lg font-semibold">Logo</div>
-          <ul className="flex space-x-8 text-white">
+      <nav className="bg-teal-900 p-5">
+        <div className="max-w-8xl text-2xl ml-8 mx-auto flex justify-between  items-center">
+          {/* Logo Image */}
+          <div className="flex items-center">
+            <img
+              src="https://i.pinimg.com/736x/c5/de/31/c5de310fc68fb65b8305fbf3d72d9682.jpg"
+              alt="Logo"
+              className="h-12 rounded-full w-auto" // You can adjust the height and width as needed
+            />
+           <p className="text-slate-100 ml-1 font-bold text-3xl"> MediForetell</p>
+          </div>
+
+          {/* Navigation Links */}
+          <ul className="flex space-x-8  text-white">
             <li>
-              <Link to="/" className="hover:text-gray-300">
+              <Link to="/" className="hover:text-gray-300 ">
                 Home
               </Link>
             </li>
@@ -18,18 +28,18 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link to="/contact" className="hover:text-gray-300">
+              <Link to="/contact" className="hover:text-gray-300 ">
                 Contact Us
               </Link>
             </li>
             <li>
               <Link to="/register">
-                <button className=" rounded">Register</button>
+                <button className="rounded">Register</button>
               </Link>
             </li>
             <li>
-            <Link to="/login">
-                <button className=" rounded">Login</button>
+              <Link to="/login">
+                <button className="rounded">Login</button>
               </Link>
             </li>
           </ul>
@@ -40,3 +50,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
